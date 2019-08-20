a8ce5ad6ca9ad0025193d0921801a5c6abda3d38
Paul Laîné <paullaine@protonmail.com>
2019-08-20 20:26:47 +0000

    Update Find-DotNetFrameworkBinaries.ps1

--- misc/Find-DotNetFrameworkBinaries.ps1
+++ misc/Find-DotNetFrameworkBinaries.ps1
@@ -1,3 +1,23 @@
+<#
+.SYNOPSIS
+    This function list all binaries that are using .Net Framework
+
+    Author: Paul Laîné (@am0nsec)
+    License: None 
+    Required Dependencies: PowerShellArsenal
+    Optional Dependencies: None
+
+.PARAMETER Location
+    The location to probe
+
+.EXAMPLE
+    Find-DotNetFrameworkBinaries -Location $env:windir\System32\*.exe
+    This shows the list all EXE that are using .Net Framework under %windir%\System32\
+
+.OUTPUTS
+    None
+    No return value
+#>
 function Find-DotNetFrameworkBinaries {
         Param(
         [parameter(Mandatory=$true)]
@@ -7,17 +27,18 @@ function Find-DotNetFrameworkBinaries {
     Write-Host "[*] Paul Laîné (@am0nsec)"
     Write-Host "[*] List all binaries that are using .Net Framework`n"
 
-    Try {
-        Import-Module PowerShellArsenal
-    } catch {
-        Write-Host "[-] PowerShellArsenal module not found!"
-        exit
+    If ($null -eq (Get-Module PowerShellArsenal)) {
+        Try {
+            Import-Module PowerShellArsenal -ErrorAction Stop
+        } Catch {
+            Write-Host "[-] PowerShellArsenal module not found!"
+            exit 1
+        }
     }
-
 
     $pes = Get-ChildItem $Location -Recurse -ErrorAction SilentlyContinue -WarningAction SilentlyContinue | Get-PE -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
     Foreach ($pe in $pes) {
-        $imports = ($pe.Imports | select ModuleName | Get-Unique -AsString).ModuleName
+        $imports = ($pe.Imports | Select-Object ModuleName | Get-Unique -AsString).ModuleName
         Foreach ($module in $imports) {
             if ($module -match "^mscor(.)+\.dll" -or $module -match "^clr\.dll$") {
                 $pe.ModuleName
